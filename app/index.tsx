da0f78e82817c9a7c0226dc30987a105177c62a9
floraExe <flora.kang0519@gmail.com>
2024-11-05 11:14:14 -0500

    Branch comp3

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.color}>
+          No recent translations
+        </Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    marginTop: 50,
+    paddingHorizontal: 20,
+
+    justifyContent : 'center',
+
+  },
+
+  box: {
+    backgroundColor: '#f9fafc',
+    paddingHorizontal: 85,
+    paddingVertical: 110,
+    borderRadius: 20
+    
+
+  },
+
+  color: {
+    color: 'grey',
+
+
+  }
+
+}
+)
